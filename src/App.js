ac71550eda4a87f2bbfa3b7380f5c275ac6d2a77
prolixmagus <jeremypfishman@gmail.com>
2024-11-27 11:17:28 -0800

    Create a new styles folder in src to hold separated css files

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import './App.css';
-import Header from './components/Header/Header.js'
-import Landing from './components/Landing/Landing.js'
-import NextCohortStarts from './components/NextCohortStarts/NextCohortStarts.js'
-import IntroStatement from './components/IntroStatement/IntroStatement.js'
-import ArticlesAndPodcasts from './components/ArticlesAndPodcasts/ArticlesAndPodcasts.js'
-import TimeToBegin from './components/TimeToBegin/TimeToBegin.js'
-import EnrollNow from './components/EnrollNow/EnrollNow.js'
-import EightPillars from './components/EightPillars/EightPillars.js'
-import Testimonials1 from './components/Testimonials1/Testimonials1.js'
-import TimeCommitment from './components/TimeCommitment/TimeCommitment.js'
-import Testimonials2 from './components/Testimonials2/Testimonials2.js'
-import AboutUs from './components/AboutUs/AboutUs.js'
-import Pricing from './components/Pricing/Pricing.js'
-import RecordingTestimonial from './components/RecordingTestimonial/RecordingTestimonial.js'
-import Footer from './components/Footer/Footer.js'
+import "./styles/site.css";
+import Header from "./components/Header/Header.js";
+import Landing from "./components/Landing/Landing.js";
+import NextCohortStarts from "./components/NextCohortStarts/NextCohortStarts.js";
+import IntroStatement from "./components/IntroStatement/IntroStatement.js";
+import ArticlesAndPodcasts from "./components/ArticlesAndPodcasts/ArticlesAndPodcasts.js";
+import TimeToBegin from "./components/TimeToBegin/TimeToBegin.js";
+import EnrollNow from "./components/EnrollNow/EnrollNow.js";
+import EightPillars from "./components/EightPillars/EightPillars.js";
+import Testimonials1 from "./components/Testimonials1/Testimonials1.js";
+import TimeCommitment from "./components/TimeCommitment/TimeCommitment.js";
+import Testimonials2 from "./components/Testimonials2/Testimonials2.js";
+import AboutUs from "./components/AboutUs/AboutUs.js";
+import Pricing from "./components/Pricing/Pricing.js";
+import RecordingTestimonial from "./components/RecordingTestimonial/RecordingTestimonial.js";
+import Footer from "./components/Footer/Footer.js";
 
-import AirtableCalendar from './components/Calendar/AirtableCalendar.js';
+import AirtableCalendar from "./components/Calendar/AirtableCalendar.js";
 // import HardcoreCalendar from './components/Calendar/hardcoreCalendar.js'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Landing />
-      <IntroStatement />
-      <ArticlesAndPodcasts />
-      <TimeToBegin />
-      <EnrollNow />
-      <NextCohortStarts />
-      <EightPillars />
-      <Testimonials1 />
+    return (
+        <div className="App">
+            <Header />
+            <Landing />
+            <IntroStatement />
+            <ArticlesAndPodcasts />
+            <TimeToBegin />
+            <EnrollNow />
+            <NextCohortStarts />
+            <EightPillars />
+            <Testimonials1 />
 
-      <AirtableCalendar /> 
-      {/* <HardcoreCalendar /> */}
+            <AirtableCalendar />
+            {/* <HardcoreCalendar /> */}
 
-      <TimeCommitment />
-      <EnrollNow />
-      <Testimonials2 />
-      <AboutUs />
-      <EnrollNow />
-      <Pricing />
-      <RecordingTestimonial />
-      <Footer />
-    </div>
-  );
+            <TimeCommitment />
+            <EnrollNow />
+            <Testimonials2 />
+            <AboutUs />
+            <EnrollNow />
+            <Pricing />
+            <RecordingTestimonial />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Calendar/Airtable.js
+++ src/components/Calendar/Airtable.js
@@ -1,25 +1,23 @@
-import axios from 'axios';
-
-
+import axios from "axios";
 
 const AIRTABLE_API_KEY = process.env.REACT_APP_AIRTABLE_API_KEY;
 const BASE_ID = process.env.REACT_APP_BASE_ID;
 const TABLE_ID = process.env.REACT_APP_TABLE_ID_WIN_CALENDAR;
 
+console.log("API key:", AIRTABLE_API_KEY);
 export const fetchData = async () => {
-  const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`;
-  const headers = { Authorization: `Bearer ${AIRTABLE_API_KEY}` };
+    const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`;
+    const headers = { Authorization: `Bearer ${AIRTABLE_API_KEY}` };
 
-  try {
-    const response = await axios.get(url, { headers });
-    return response.data.records.map(record => ({
-      pillarName: record.fields.PillarSection,
-      displayedDate: record.fields.DisplayedDate,
-      // date: record.fields.Date,
-      
-    }));
-  } catch (error) {
-    console.error('Error fetching data from Airtable', error);
-    return [];
-  }
+    try {
+        const response = await axios.get(url, { headers });
+        return response.data.records.map((record) => ({
+            pillarName: record.fields.PillarSection,
+            displayedDate: record.fields.DisplayedDate,
+            // date: record.fields.Date,
+        }));
+    } catch (error) {
+        console.error("Error fetching data from Airtable", error);
+        return [];
+    }
 };
