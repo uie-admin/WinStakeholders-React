029e1beaaf1817570f5ecc860b8fcc39c0985c6b
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-12-12 12:21:03 -0500

    nextcohortstart update

--- src/components/NextCohortStarts/NextCohortStarts.js
+++ src/components/NextCohortStarts/NextCohortStarts.js
@@ -3,12 +3,12 @@ import { STARTING_DATE } from "../../const.js";
 
 function NextCohortStarts() {
     return (
-        <div
+        <h2
             id="nextCohortStarts"
             className="startingDate text-center text-white"
         >
             Next cohort starts: <strong>{STARTING_DATE[0].date}</strong>
-        </div>
+        </h2>
     );
 }
 
